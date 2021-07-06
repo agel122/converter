11f3ea526b006e2e60d739d75cbf918dccce4747
agel122 <agel122@yandex.ru>
2021-07-06 13:39:19 +0300

    with django-forms

--- converter/forms.py
+++ converter/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class Adder(forms.Form):
-    added_data = forms.CharField(label='Added_Data', max_length=50)
+    added_data = forms.CharField(label='weight in grams', error_messages={'required': 'input your data'}, max_length=50)
