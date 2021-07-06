ecc30ddc90f0a6124bf8ec7e0cf972cb03ea1d49
agel122 <agel122@yandex.ru>
2021-07-06 15:08:19 +0300

    added exceptions

--- converter/views.py
+++ converter/views.py
@@ -10,9 +10,14 @@ def weigth_converter(request):
     if request.method == 'POST':
         form = Adder(request.POST)
         if form.is_valid():
-            value_in_g = int(form.cleaned_data['added_data'])
-            result_kg = value_in_g/1000
-            return render(request, 'weight_converter.html', {'result': result_kg, 'initial_value': value_in_g,})
+            try:
+                value_in_g = int(form.cleaned_data['added_data'])
+                result_kg = value_in_g/1000
+                return render(request, 'weight_converter.html',
+                              {'result': result_kg, 'initial_value': value_in_g,})
+            except ValueError as e:
+                e.message = 'Enter the number, not the string'
+                return render(request, 'weight_converter.html', {'message': e.message})
     else:
         form = Adder()
     return render(request, 'weight_converter.html', {'form': form})
