ecb2bdc833ff495c9c7336298e67a85762cf187d
agel122 <agel122@yandex.ru>
2021-07-12 21:07:34 +0300

    dispatcher has been improved

--- converter/converter_type.py
+++ converter/converter_type.py
@@ -14,3 +14,6 @@ dispatcher = {'weight_converter': weight_converter}
 
 
 
+
+
+
--- converter/views.py
+++ converter/views.py
@@ -15,10 +15,7 @@ def converter(request):
                 converter_name=form.cleaned_data['selected_converter']
                 value_to_convert = int(form.cleaned_data['added_data'])
                 return render(request, 'converter.html',
-                              {'result': dispatcher[converter_name](value_to_convert)['result'],
-                               'initial_value': value_to_convert,
-                               'initial_unit': dispatcher[converter_name](value_to_convert)['initial_unit'],
-                               'result_unit': dispatcher[converter_name](value_to_convert)['result_unit']})
+                              dispatcher[converter_name](value_to_convert))
             except ValueError as e:
                 e.message = 'Enter the number, not the string'
                 return render(request, 'converter.html', {'message': e.message})
